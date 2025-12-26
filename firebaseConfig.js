9ac180965e4845e8be1ef3290e1171f2b1731372
Eshaan Tripathi <68991885+BeanQueso@users.noreply.github.com>
2025-12-25 19:21:32 -0600

    Redact Firebase config details
    Redacted sensitive Firebase configuration details for security.

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -4,13 +4,7 @@ const { initializeAuth, getReactNativePersistence } = require('firebase/auth');
 const AsyncStorage = require('@react-native-async-storage/async-storage');
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDX0-DiQJUmQz4quhZri3d-c7LqGvHffQs",
-  authDomain: "naloxonelocator-1a92e.firebaseapp.com",
-  projectId: "naloxonelocator-1a92e",
-  storageBucket: "naloxonelocator-1a92e.appspot.com",
-  messagingSenderId: "160570111820",
-  appId: "1:160570111820:web:2665cd28c7e7507dd581e9",
-  measurementId: "G-E7XHRC189F"
+  //////////// Redacted for Security ////////////
 };
 
 const app = initializeApp(firebaseConfig);
